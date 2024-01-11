e0bb286cef5b788f10d74903f2fcc36e0860fad3
Assiya zee <assiyazee@gmail.com>
2024-01-11 12:30:06 -0800

    OUR-1 is given for project

---
+++ src/PracticeDay1/_01_OUR.java
@@ -0,0 +1,31 @@
+package PracticeDay1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_OUR {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.lidl.com");
+
+        WebElement seachBox = driver.findElement(By.id("seach-bar"));
+        seachBox.sendKeys("Your seach query");
+        seachBox.submit();
+        try {
+            Thread.sleep((5000));
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
+
+
+
+
+
+
